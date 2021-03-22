25b7b689d688db05883316441bec8c354da861db
Christian Haugen <chaugen55@gmail.com>
2021-03-22 00:49:48 -0700

    adding new logic to change button color if user has learned a topic

--- learn_python/Student.py
+++ learn_python/Student.py
@@ -1,29 +1,44 @@
 import logging as log
 from LearningModule import *
 
-curriculum =  ['Numbers', 'Variables', 'Strings', 'Lists', 'Booleans',
-              'Dictionaries', 'Conditionals', 'Operators: And, Or, Not',
-              'Operators: Comparison', 'For Loops', 'While Loops', 
-              'Functions', 'Arguments', 'Scope', 'Libraries']
+curriculum = [
+    {'class_name': 'Numbers'},
+    {'class_name': 'Variables'},
+    {'class_name': 'Strings'},
+    {'class_name': 'Lists'},
+    {'class_name': 'Booleans'},
+    {'class_name': 'Dictionaries'},
+    {'class_name': 'Conditionals'},
+    {'class_name': 'Operators: And, Or, Not'},
+    {'class_name': 'Operators: Comparison'},
+    {'class_name': 'For Loops'},
+    {'class_name': 'While Loops'},
+    {'class_name': 'Functions'},
+    {'class_name': 'Arguments'},
+    {'class_name': 'Scope'},
+    {'class_name': 'Libraries'}
+]
 
-numbers_module =                LearningModule(moduleName = "Numbers", topicsCovered = ["Integers", "Floats", "Simple math", "+=, -=. *=, /="])
-variables_module =              LearningModule(moduleName = "Variables", topicsCovered = ["Variables: The basics", "The DRY Method", "Variables: Conventions"])
-strings_module =                LearningModule(moduleName = "Strings", topicsCovered = ["String indexing", "String slicing", "Strings: Properties", "Strings Methods", "Print formatting"])
-lists_module =                  LearningModule(moduleName = "Lists", topicsCovered = ["Lists: The basics", "Covered", "List indexing"])
-booleans_module =               LearningModule(moduleName = "Booleans", topicsCovered = ["Booleans: The basics"])
-dictionaries_module =           LearningModule(moduleName = "Dictionaries", topicsCovered = ["Dictionaries: The basics", "Dictionary referencing"])
-conditionals_module =           LearningModule(moduleName = "Conditionals", topicsCovered = ["If, Else, and Elif", "Nesting conditionals (and why you shouldn't)"])
-operators_and_or_not_module =   LearningModule(moduleName = "Operators: And, Or, Not", topicsCovered = ["And, Or, and Not", "Chaining operators"])
-operators_comparison_module =   LearningModule(moduleName = "Operators: Comparison", topicsCovered = ["Comparison operartors"])
-for_loops_module =              LearningModule(moduleName = "For Loops", topicsCovered = ["For Loops: The basics"])
-while_loops_module =            LearningModule(moduleName = "While Loops", topicsCovered = ["While Loops: The basics"])
-functions_module =              LearningModule(moduleName = "Functions", topicsCovered = ["Why functions are so useful", "Functions: Conventions", "Returning values", "Paramaters: Setting rules for your function", "Type hinting your parameters"])
-arguments_module =              LearningModule(moduleName = "Arguments", topicsCovered = ["Arguments? Don't you mean paramaters? (NO!)", "Arguments: Conventions"])
-scope_module =                  LearningModule(moduleName = "Scope", topicsCovered = ["Local vs. Global Scope", "Global Variables: Conventions"])
-libraries_module =              LearningModule(moduleName = "Libraries", topicsCovered = ["Using your programming to change the world"])
+numbers_module = LearningModule(moduleName="Numbers", topicsCovered=["Integers", "Floats", "Simple math", "+=, -=. *=, /="])
+variables_module = LearningModule(moduleName="Variables", topicsCovered=["Variables: The basics", "The DRY Method", "Variables: Conventions"])
+strings_module = LearningModule(moduleName="Strings", topicsCovered=["String indexing", "String slicing", "Strings: Properties", "Strings Methods", "Print formatting"])
+lists_module = LearningModule(moduleName="Lists", topicsCovered=["Lists: The basics", "Covered", "List indexing"])
+booleans_module = LearningModule(moduleName="Booleans", topicsCovered=["Booleans: The basics"])
+dictionaries_module = LearningModule(moduleName="Dictionaries", topicsCovered=["Dictionaries: The basics", "Dictionary referencing"])
+conditionals_module = LearningModule(moduleName="Conditionals", topicsCovered=["If, Else, and Elif", "Nesting conditionals (and why you shouldn't)"])
+operators_and_or_not_module = LearningModule(moduleName="Operators: And, Or, Not", topicsCovered=["And, Or, and Not", "Chaining operators"])
+operators_comparison_module = LearningModule(moduleName="Operators: Comparison", topicsCovered=["Comparison operartors"])
+for_loops_module = LearningModule(moduleName="For Loops", topicsCovered=["For Loops: The basics"])
+while_loops_module = LearningModule(moduleName="While Loops", topicsCovered=["While Loops: The basics"])
+functions_module = LearningModule(moduleName="Functions", topicsCovered=["Why functions are so useful", "Functions: Conventions", "Returning values", "Paramaters: Setting rules for your function", "Type hinting your parameters"])
+arguments_module = LearningModule(moduleName="Arguments", topicsCovered=["Arguments? Don't you mean paramaters? (NO!)", "Arguments: Conventions"])
+scope_module = LearningModule(moduleName="Scope", topicsCovered=["Local vs. Global Scope", "Global Variables: Conventions"])
+libraries_module = LearningModule(moduleName="Libraries", topicsCovered=["Using your programming to change the world"])
+
+
 class Student:
     progress = 0
-    topics_learned = {
+    topics_to_learn = {
         'Numbers':                  {'has_been_learned': False},
         'Variables':                {'has_been_learned': False},
         'Strings':                  {'has_been_learned': False},
@@ -45,22 +60,38 @@ class Student:
         self.name = name
         self.is_beginner = is_beginner
 
+    def update_button_colors_for_learned_topics(self):
+        for c in curriculum:
+            if self.topics_to_learn[c['class_name']]['has_been_learned'] == True:
+                c['button_color'] = 'green'
+            else:
+                c['button_color'] = 'red'
+
+
     def has_learned_topic(self, topic: str):
-        if topic not in curriculum:
+        if topic not in self.topics_to_learn.keys():
             log.warning(
                 f'Looks like "{topic}" isn\'t in this curriculum . . . Email chaugen@apple.com to see if Christian can add it!')
             return
         else:
             print(f'Nice job learning "{topic}"!')
-            self.topics_learned[topic]['has_been_learned'] = True
+            self.topics_to_learn[topic]['has_been_learned'] = True
+            self.update_button_colors_for_learned_topics()
 
     def has_learned_multiple_topics(self, topics: list):
         for topic in topics:
             self.has_learned_topic(topic)
 
+    def learned_topics(self):
+        results = []
+        for topic in self.topics_to_learn.items():
+            if topic['has_been_learned'] == True:
+                results.append(topic.key())
+        return results
+
     def print_topics_learned_so_far(self):
         topics = []
-        for topic, value_dict in self.topics_learned.items():
+        for topic, value_dict in self.topics_to_learn.items():
             if value_dict['has_been_learned'] == True:
                 topics.append(topic)
 
@@ -73,11 +104,11 @@ class Student:
         print(statement + ' Awesome work!')
 
     def print_total_progress(self):
-        topics_learned_count = 0
-        for topic, value_dict in self.topics_learned.items():
+        topics_to_learn_count = 0
+        for topic, value_dict in self.topics_to_learn.items():
             if value_dict['has_been_learned'] == True:
-                topics_learned_count += 1
-        self.progress = (topics_learned_count / len(self.topics_learned)) * 100
+                topics_to_learn_count += 1
+        self.progress = (topics_to_learn_count / len(self.topics_to_learn)) * 100
         print(f'Total progress: {self.progress}%')
 
     def save_learning_progress(self):
@@ -92,5 +123,5 @@ class Student:
 # christian.has_learned_multiple_topics( ['Strings', 'Lists', 'Functions'])
 # christian.print_total_progress()
 # print(christian.progress)
-# christian.print_topics_learned_so_far()
+# christian.print_topics_to_learn_so_far()
 # print(christian.is_beginner)
--- learn_python/learn.py
+++ learn_python/learn.py
@@ -1,20 +1,43 @@
 import tkinter as tk
-from get_user_input, Student, LearningModule import *
+from get_user_input import *
+from Student import * 
+from LearningModule import *
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
+    def say_hi(self):
+        print("hi there, everyone!")
+
+    def say_name_of_module(self):
+        print(self.name['text'])
+
+    def createWidgets(self):
+        self.QUIT = tk.Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+        self.QUIT.pack({"side": "left"})
+
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello",
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack({"side": "left"})
+
+
+        for topic in self.topic_buttons_to_create:
+            name = str(topic['class_name'])
+            side = "top"
+            self.name = tk.Button(self)
+            self.name['text'] = "{}".format(name)
+            self.name['command'] = self.say_name_of_module
+            self.name['fg'] = topic['button_color']
+            self.name.pack({'side' : side})
+
+    def __init__(self, master=None, topic_buttons_to_create: list = [], modules_student_has_learned: list = []):
+        tk.Frame.__init__(self, master)
+        self.topic_buttons_to_create = topic_buttons_to_create
+        self.modules_student_has_learned = modules_student_has_learned
         self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.say_hi = tk.Button(self)
-        self.say_hi =['text'] = 'Hello, world! \n(click me)'
-        self.say_hi =['command'] = self.welcome
-
-    def welcome(self):
-        print('Hey there, everyone!')
+        self.createWidgets()
 
 
 user = get_user_name()
@@ -26,11 +49,10 @@ student.has_learned_topic('Puppies')
 student.has_learned_multiple_topics(topics =['Strings', 'Lists', 'Functions'])
 student.print_total_progress()
 print(student.progress)
-student.print_topic_learned_so_far()
+student.print_topics_learned_so_far()
 print(student.is_beginner)
 
 
 root = tk.Tk(className="Let's learn Python!")
-
-
-# root.mainloop()
+app = Application(master=root, topic_buttons_to_create=curriculum, modules_student_has_learned= student.learned_topics)
+app.mainloop()
