b646eabeb57eb11bb93b80548d5692a9441ab59b
Christian Haugen <chaugen55@gmail.com>
2021-04-01 23:32:18 -0700

    test commit message for zshrc

--- learn_python/learn.py
+++ learn_python/learn.py
@@ -15,39 +15,40 @@ class Application(tk.Frame):
 
     def createWidgets(self):
         self.PYTHON_IMAGE = tk.PhotoImage(name='python.png')
+        boldFont = tk.BOLD(size = 10, weight = "bold")
 
         self.QUIT = tk.Button(self, text='QUIT', fg='red', command=self.quit).grid(row=7 , column=4)    
 
         # 1st section
-        self.easy_stuff =               tk.Label(self, text='Easy concepts').grid(row=1, column=1)
+        self.easy_stuff =               tk.Label(self, text='Easy concepts', weight= 'bold', font=boldFont).grid(row=1, column=1)
 
-        self.numbers =                  tk.Button(self, text='Numbers', command=self.say_hi).grid(row=2, column=1)
-        self.variables =                tk.Button(self, text='Variables', command=self.say_hi).grid(row=2, column=2)
-        self.strings =                  tk.Button(self, text='Strings', command=self.say_hi).grid(row=2, column=3)
-        self.lists =                    tk.Button(self, text='Lists', command=self.say_hi).grid(row=2, column=4)
-        self.booleans =                 tk.Button(self, text='Booleans', command=self.say_hi).grid(row=2, column=5)
-        self.dictionaries =             tk.Button(self, text='Dictionaries', command=self.say_hi).grid(row=2, column=6)
+        self.numbers =                  tk.Button(self, text='Numbers', command=self.say_hi, fg='green').grid(row=2, column=1)
+        self.variables =                tk.Button(self, text='Variables', command=self.say_hi, fg='green').grid(row=2, column=2)
+        self.strings =                  tk.Button(self, text='Strings', command=self.say_hi, fg='green').grid(row=2, column=3)
+        self.lists =                    tk.Button(self, text='Lists', command=self.say_hi, fg='green').grid(row=2, column=4)
+        self.booleans =                 tk.Button(self, text='Booleans', command=self.say_hi, fg='green').grid(row=2, column=5)
+        self.dictionaries =             tk.Button(self, text='Dictionaries', command=self.say_hi, fg='green').grid(row=2, column=6)
 
 
 
         # 2nd section
-        self.not_difficult_label =      tk.Label(self, text="This content isn't difficult either").grid(row=3, column=1)
+        self.not_difficult_label =      tk.Label(self, text="This content isn't difficult either", font=boldFont, fg= 'blue').grid(row=3, column=1)
 
-        self.conditionals =             tk.Button(self, text='Conditionals', command=self.say_hi).grid(row=4, column=2)
-        self.operators_and_or_not =     tk.Button(self, text='Operators: And, Or, Not', command=self.say_hi).grid(row=4, column=3)
-        self.operators_comparison =     tk.Button(self, text='Operators: Comparison', command=self.say_hi).grid(row=4, column=4)
+        self.conditionals =             tk.Button(self, text='Conditionals', command=self.say_hi, fg='yellow').grid(row=4, column=2)
+        self.operators_and_or_not =     tk.Button(self, text='Operators: And, Or, Not', command=self.say_hi, fg='yellow').grid(row=4, column=3)
+        self.operators_comparison =     tk.Button(self, text='Operators: Comparison', command=self.say_hi, fg='yellow').grid(row=4, column=4)
 
 
 
         # 3rd section
-        self.youre_almost_done_label =  tk.Label(self, text="You're almost done, then your programming career begins").grid(row=5, column=1)
+        self.youre_almost_done_label =  tk.Label(self, text="You're almost done, then your programming career begins", font=boldFont, fg= 'blue').grid(row=5, column=1)
 
-        self.for_loops =                tk.Button(self, text='For Loops', command=self.say_hi).grid(row=6, column=1)
-        self.while_loops =              tk.Button(self, text='While Loops', command=self.say_hi).grid(row=6, column=2)
-        self.functions =                tk.Button(self, text='Functions', command=self.say_hi).grid(row=6, column=3)
-        self.arguments =                tk.Button(self, text='Arguments', command=self.say_hi).grid(row=6, column=4)
-        self.scope =                    tk.Button(self, text='Scope', command=self.say_hi).grid(row=6, column=5)
-        self.libraries =                tk.Button(self, text='Libraries', command=self.say_hi).grid(row=6, column=6)
+        self.for_loops =                tk.Button(self, text='For Loops', command=self.say_hi, fg= 'red').grid(row=6, column=1)
+        self.while_loops =              tk.Button(self, text='While Loops', command=self.say_hi, fg= 'red').grid(row=6, column=2)
+        self.functions =                tk.Button(self, text='Functions', command=self.say_hi, fg= 'red').grid(row=6, column=3)
+        self.arguments =                tk.Button(self, text='Arguments', command=self.say_hi, fg= 'red').grid(row=6, column=4)
+        self.scope =                    tk.Button(self, text='Scope', command=self.say_hi, fg= 'red').grid(row=6, column=5)
+        self.libraries =                tk.Button(self, text='Libraries', command=self.say_hi, fg= 'red').grid(row=6, column=6)
 
     def __init__(self, master=None, topic_buttons_to_create: list = [], modules_student_has_learned: list = []):
         tk.Frame.__init__(self, master)
